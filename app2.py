e03f7f0977ec2a4f0f5467520719a4ee77ba994d
HimanshiiG <gaurmansi3657@gmail.com>
2024-10-06 18:23:32 +0530

    init: Question Generator and AI Answer detector

---
+++ app2.py
@@ -0,0 +1,269 @@
+import streamlit as st
+import PyPDF2
+import os
+from streamlit import session_state
+import time
+import random
+import mysql.connector
+import pickle
+
+with open('./pkl_files/AutoTokenizer.pkl', 'rb') as file:
+    tokenizer = pickle.load(file)
+with open('./pkl_files/AutoModelForSeq2SeqLM.pkl', 'rb') as file:
+    model = pickle.load(file)
+session_state['page']="Login"
+session_state.authenticated = False
+actual_answers = []
+#PDF to TEXT
+def uploadpdf():
+    st.title("PDF To Text")
+    uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
+    if uploaded_file is not None:
+        with open("temp.pdf", "wb") as temp_file:
+            temp_file.write(uploaded_file.getvalue())
+        extracted_text = extract_text_from_pdf("temp.pdf")
+        st.write(f"Text Extracted from PDF:")
+        st.text(extracted_text)
+        os.remove("temp.pdf")
+        add_text_to_text_table(extracted_text)
+    
+def extract_text_from_pdf(pdf_file):
+    with open(pdf_file, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
+        return text
+# MYSQL Connection and Queries
+def create_connection():
+    """Create a connection to the MySQL database."""
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database="kj"
+        )
+        return connection
+    except mysql.connector.Error as e:
+        st.error(f"Error: {e}")
+        return None
+
+def get_last_text_execute_query(connection = create_connection(), query = "SELECT whole_text FROM Text ORDER BY id DESC LIMIT 1;"):
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    return result[0]["whole_text"]
+def add_text_to_text_table(text_data):
+    connection = create_connection()
+    if connection:
+        try:
+            # Create a cursor object
+            cursor = connection.cursor()
+
+            # SQL query to insert text data into the text table
+            query = "INSERT INTO Text (whole_text) VALUES (%s);"
+
+            # Execute the query with the text data
+            cursor.execute(query, (text_data,))
+
+            # Commit the changes
+            connection.commit()
+
+            # Close the cursor (the connection remains open)
+            cursor.close()
+
+            st.success("Data added successfully.")
+        except mysql.connector.Error as e:
+            st.error(f"Error: {e}")
+        finally:
+            # Close the connection
+            connection.close()
+
+
+def insert_question_answer_pairs_query(questions, answers, identifier):
+    connection = create_connection()
+    
+    if connection:
+        try:
+            # Create a cursor object
+            cursor = connection.cursor()
+
+            # SQL query to insert question-answer pairs into the database
+            query = "INSERT INTO qa (question, answer, identifier) VALUES (%s, %s, %s);"
+
+            # Check if the lengths of questions and answers are the same
+            if len(questions) != len(answers):
+                st.error("Number of questions and answers must be the same.")
+                return
+
+            # Execute the query for each question-answer pair
+            for q, a in zip(questions, answers):
+                cursor.execute(query, (q, a, identifier))
+
+            # Commit the changes
+            connection.commit()
+
+            # Close the cursor (the connection remains open)
+            cursor.close()
+
+            st.success("Data added successfully.")
+        except mysql.connector.Error as e:
+            st.error(f"Error: {e}")
+        finally:
+            # Close the connection
+            connection.close()
+
+#Questions Page and its functions
+def GetQuestions():
+        st.header("Curated Questions")
+        
+        data = get_last_text_execute_query()
+        if data:
+            questions = questions_generator_driver(data)
+            answers = display_questions(questions)
+            # st.write(answers)
+                
+
+def questions_generator_driver(data):
+    document = data
+    # Split the document into smaller chunks for batch processing
+    chunk_size = 800  # You may need to adjust this based on your document size
+    document_chunks = [document[i:i + chunk_size] for i in range(0, len(document), chunk_size)]
+
+    num_broader_questions = 2
+    num_niche_questions = 1
+
+    broader_questions = generate_random_questions_batch(document_chunks, num_broader_questions)
+    broader_final_questions = random.sample(broader_questions, k=2)
+    niche_questions = generate_random_questions_batch(document_chunks, num_niche_questions)
+    niche_final_questions = random.sample(niche_questions, k=1)
+    questions = []
+    for question in broader_final_questions:
+                questions.append(question)
+    for question in niche_final_questions:
+                questions.append(question)
+    return questions
+
+def generate_random_questions_batch(documents, num_questions):
+    questions = []
+
+    for document in documents:
+        if len(document.split()) <= 1:
+            continue  # Skip empty or very short documents
+
+        text_lengths = [random.randint(1, min(len(document.split()) - 1, 10)) for _ in range(2)]
+
+        start_indices = [random.randint(0, len(document.split()) - text_length - 1) for text_length in text_lengths]
+
+        input_texts = [
+            " ".join([
+                document.split()[i] if i < start_index or i >= start_index + text_length else "[HL]" 
+                for i in range(len(document.split()))
+            ]) for start_index, text_length in zip(start_indices, text_lengths)
+        ]
+
+        temp_answers = []
+
+        for input_text in input_texts:
+            temp = input_text.split()
+            while "[HL]" in temp:
+                temp.remove("[HL]")
+            string = " ".join(temp)
+            temp_answers.append(string)
+
+        actual_answers.extend(temp_answers)
+
+        input_ids_list = [tokenizer(input_text, return_tensors="pt").input_ids for input_text in input_texts]
+
+        # Assuming model input limit is 512 tokens, you may need to adjust this based on your model
+        max_tokens = 512
+
+        for input_ids in input_ids_list:
+            # Split input_ids into chunks of size max_tokens
+            for i in range(0, len(input_ids[0]), max_tokens):
+                input_ids_chunk = input_ids[:, i:i + max_tokens]
+
+                # Generate output for each chunk
+                output = model.generate(input_ids_chunk)
+
+                # Decode the generated output
+                generated_question = tokenizer.decode(output[0], skip_special_tokens=True)
+                questions.append(generated_question)
+
+    return questions
+def display_questions(questions):
+    # Initialize empty list to store user responses
+    user_responses = []
+
+    # Display each question and input field
+    for i, question in enumerate(questions, start=1):
+            response = st.text_area(f"Question {i}: {question}")
+            # user_responses.append(response)
+    if st.button("Submit"):
+        insert_question_answer_pairs_query(questions, answers, 0)
+    # Display the user responses
+    # st.subheader("Your Responses:")
+    # font_size = 24
+    # for i, response in enumerate(user_responses, start=1):
+    #     st.markdown(f"<p style='font-size: {font_size}px;'>Question {i}: {response}</p>", unsafe_allow_html=True)
+        # st.markdown(f"<p style='font-size: 32px;'>Question {i}: {response} </p>")
+
+#Login Page
+
+def login(username, password):
+    return username == "neil" and password == "neil"
+def loginPage():
+    # Create a session_state state to store user authentication status
+    session_state.username=""
+
+    if not session_state.authenticated:
+        # Display login form if not authenticated
+        st.subheader("Login")
+
+        # User input for username and password
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+
+        # Login button
+        if st.button("Login"):
+            if login(username, password):
+                # Update session_state state on successful login
+                session_state.username = username
+                session_state.authenticated = True
+                st.experimental_set_query_params(authenticated=True)
+                st.success("Login successful! Redirecting...")
+                loggedin =True
+
+            else:
+                st.error("Invalid username or password. Please try again.")
+    else:
+        # Redirect to another page if authenticated
+        st.experimental_set_query_params(authenticated=True)
+        st.experimental_rerun()
+
+        # Display content for authenticated users
+        st.subheader(f"Welcome, {session_state.username}!")
+        st.write("This is the authenticated content.")
+
+
+def main():
+    # st.title("Welcome to AI Powered Test")
+    page = st.sidebar.radio("Select Page", [ "Upload", "Questions"])
+    session_state['page'] = str(page)
+    # if navigation() == "login":
+    #     loginPage()
+    # if navigation() == "upload":
+    #     uploadpdf()
+    # if navigation() == "questions":
+    #     GetQuestions()
+    if session_state['page'] == "Upload":
+        uploadpdf()
+    elif session_state['page'] == "Login":
+        loginPage()
+    elif session_state['page'] == "Questions":
+        GetQuestions()
+if __name__ == "__main__":
+    main()
